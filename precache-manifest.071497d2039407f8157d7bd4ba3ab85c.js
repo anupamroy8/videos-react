3ba8d3dfcd99e524de4c090efa39206807e22a50
anupamroy8 <anupamroy8@gmail.com>
2020-09-18 18:10:25 +0530

    Updates

---
+++ precache-manifest.071497d2039407f8157d7bd4ba3ab85c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e15b5b7b2052528ab84d8a76d713fd9",
+    "url": "/videos-react/index.html"
+  },
+  {
+    "revision": "4470329ad80f14123ca6",
+    "url": "/videos-react/static/css/main.e663cc3c.chunk.css"
+  },
+  {
+    "revision": "2df4f36172a539770d83",
+    "url": "/videos-react/static/js/2.0079a71e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/videos-react/static/js/2.0079a71e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4470329ad80f14123ca6",
+    "url": "/videos-react/static/js/main.f879bfe2.chunk.js"
+  },
+  {
+    "revision": "ad91e9cbe2cf8a65969e",
+    "url": "/videos-react/static/js/runtime-main.f2152d45.js"
+  }
+]);
